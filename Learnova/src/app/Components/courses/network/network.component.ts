3022b61c08c041fcc5d539d0b093900994031fad
Moustafa Mahmoud <moustafamahmoud5600@gmail.com>
2026-01-05 11:55:22 +0200

    @Moustafa

--- Learnova/src/app/Components/courses/network/network.component.ts
+++ Learnova/src/app/Components/courses/network/network.component.ts
@@ -1,14 +1,35 @@
 
-import { Component, Input } from '@angular/core';
+import { Component, effect, inject, Input } from '@angular/core';
 import { CartService } from '../../../../services/cartser/cart.service';
+import { CoursesService } from '../../../../services/courses_ser/courses.service';
+import { map } from 'rxjs';
+import { ICourses } from '../icourses';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-network',
-    imports: [],
-    templateUrl: './network.component.html',
-    styleUrl: './network.component.css'
+  selector: 'app-network',
+  imports: [CommonModule],
+  templateUrl: './network.component.html',
+  styleUrl: './network.component.css',
 })
 export class NetworkComponent {
+  private Courses_services = inject(CoursesService);
 
+  private $network = this.Courses_services.Allcourses().pipe(
+    map((courses) => courses ?? ([] as ICourses[])),
+    map((courses) =>
+      courses.filter(
+        (courses) => courses.typ?.toLocaleLowerCase() === 'network'
+      )
+    )
+  );
 
+  _network = toSignal(this.$network, { initialValue: [] as ICourses[] });
+
+  constructor() {
+    effect(() => {
+      console.log(this._network());
+    });
+  }
 }
