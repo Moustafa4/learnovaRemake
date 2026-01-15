84f2f20273a9dbc9a2055fbd146cd3c273cec81b
Moustafa Mahmoud <moustafamahmoud5600@gmail.com>
2026-01-15 11:19:46 +0200

    @Moustafa

--- Learnova/src/app/Components/header2/header2.component.ts
+++ Learnova/src/app/Components/header2/header2.component.ts
@@ -1,17 +1,61 @@
-
-import { Component, HostListener } from '@angular/core';
-import { RouterLinkActive, RouterModule } from '@angular/router';
+import { Component, HostListener, inject } from '@angular/core';
+import { Router, RouterLinkActive, RouterModule } from '@angular/router';
+import { CoursesService } from '../../../services/courses_ser/courses.service';
+import { map } from 'rxjs';
+import { ICourses } from '../courses/icourses';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-header2',
-    imports: [RouterModule, RouterLinkActive],
-    templateUrl: './header2.component.html',
-    styleUrl: './header2.component.css'
+  selector: 'app-header2',
+  imports: [RouterModule, RouterLinkActive, FormsModule, CommonModule],
+  templateUrl: './header2.component.html',
+  styleUrl: './header2.component.css',
 })
 export class Header2Component {
-isScrolled=false;
-@HostListener('window:scroll')
-  onScroll(){
-    this.isScrolled=window.scrollY>10;
-}
+  private router=inject(Router)
+  private courses_services = inject(CoursesService);
+  private $courses = this.courses_services
+    .Allcourses()
+    .pipe(map((courses) => courses ?? ([] as ICourses[])));
+  private _courses = toSignal(this.$courses, {
+    initialValue: [] as ICourses[],
+  });
+  showOverlay = false;
+
+  sear_word: string = '';
+  searchfilter: ICourses[] = this._courses();
+  onsearch(searchval: string) {
+    const value = this.sear_word.trim().toLowerCase();
+
+    if (!value) {
+      this.showOverlay = false;
+      this.searchfilter = [];
+      return;
+    }
+
+    this.searchfilter = this._courses().filter((c) =>
+      c.title.toLowerCase().includes (value)
+    );
+
+    this.showOverlay = true;
+  }
+  goToCourse(title: string) {
+    this.showOverlay = false;
+    this.router.navigate(['Details/', title]);
+  }
+
+  isScrolled = false;
+  @HostListener('window:scroll')
+  onScroll() {
+    this.isScrolled = window.scrollY > 10;
+  }
+  @HostListener('document:click', ['$event'])
+  closeOverlay(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.search-wrapper')) {
+      this.showOverlay = false;
+    }
+  }
 }
